963dd2e813f320a7a49189616c77f250d686883c
jinkazama13200 <jinkazama13200@gmail.com>
2024-01-09 18:09:25 +0700

    updated 9-1

--- src/components/discordProfile/Main.jsx
+++ src/components/discordProfile/Main.jsx
@@ -1,17 +1,100 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useEffect, useMemo, useState } from "react";
 import { Container, styled } from "@mui/material";
+import Particles, { initParticlesEngine } from "@tsparticles/react";
+import { loadFull } from "tsparticles";
 import Profile from "./Profile/Profile";
 
-const Background = styled("div")`
-  background-image: url("https://i.pinimg.com/originals/8e/46/15/8e46150f790fbefe438d9c2767c32ad1.gif");
-  background-repeat: round;
-  background-position: center;
-  height: 100vh;
-`;
 export default function Main() {
-  return (
-    <Background>
-      <Profile />
-    </Background>
+  const [init, setInit] = useState(false);
+
+  // -----
+  useEffect(() => {
+    initParticlesEngine(async (engine) => {
+      await loadFull(engine);
+    }).then(() => {
+      setInit(true);
+    });
+  }, []);
+
+  const options = useMemo(
+    () => ({
+      background: {
+        color: {
+          value: "#856886",
+        },
+      },
+      fpsLimit: 120,
+      interactivity: {
+        events: {
+          onClick: {
+            enable: true,
+            mode: "push",
+          },
+          onHover: {
+            enable: true,
+            mode: "repulse",
+          },
+        },
+        modes: {
+          push: {
+            quantity: 1,
+          },
+          repulse: {
+            distance: 200,
+            duration: 0.4,
+          },
+        },
+      },
+      particles: {
+        color: {
+          value: "#ffffff",
+        },
+        links: {
+          color: "#ffffff",
+          distance: 150,
+          enable: true,
+          opacity: 1,
+          width: 1,
+        },
+        move: {
+          direction: "none",
+          enable: true,
+          outModes: {
+            default: "bounce",
+          },
+          random: true,
+          speed: 6,
+          straight: true,
+        },
+        number: {
+          density: {
+            enable: true,
+          },
+          value: 80,
+        },
+        opacity: {
+          value: 1,
+        },
+        shape: {
+          type: "circle",
+        },
+        size: {
+          value: { min: 1, max: 5 },
+        },
+      },
+      detectRetina: true,
+    }),
+    []
   );
+
+  if (init) {
+    return (
+      <Fragment>
+        <Particles id="tsparticles" options={options} />
+        <Profile />
+      </Fragment>
+    );
+  }
+
+  return <></>;
 }
